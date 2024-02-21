513cad29805ce3f5abb94d7002b6111ecee633a8
rizamoyi <samwelmoyi@gmail.com>
2024-02-21 11:50:32 +0300

    project setup

---
+++ src/lib/domain/clip/mod.rs
@@ -0,0 +1,12 @@
+pub mod field;
+
+pub struct Clip {
+  pub clip_id: field::ClipId,
+  pub shortcode: field::ShortCode,
+  pub content: field::Content,
+  pub title: field::Title,
+  pub posted: field::Posted,
+  pub expires: field::Expires,
+  pub password: field::Password,
+  pub hits: field::Hits,
+}
---
+++ target/debug/deps/clipstash-231b751fcb4cc9f3.d
@@ -0,0 +1,11 @@
+/home/samkach/Desktop/clipstash/target/debug/deps/libclipstash-231b751fcb4cc9f3.rmeta: src/lib/mod.rs src/lib/data/mod.rs src/lib/domain/mod.rs src/lib/domain/clip/mod.rs src/lib/domain/clip/field/mod.rs src/lib/service/mod.rs src/lib/web/mod.rs
+
+/home/samkach/Desktop/clipstash/target/debug/deps/clipstash-231b751fcb4cc9f3.d: src/lib/mod.rs src/lib/data/mod.rs src/lib/domain/mod.rs src/lib/domain/clip/mod.rs src/lib/domain/clip/field/mod.rs src/lib/service/mod.rs src/lib/web/mod.rs
+
+src/lib/mod.rs:
+src/lib/data/mod.rs:
+src/lib/domain/mod.rs:
+src/lib/domain/clip/mod.rs:
+src/lib/domain/clip/field/mod.rs:
+src/lib/service/mod.rs:
+src/lib/web/mod.rs:
